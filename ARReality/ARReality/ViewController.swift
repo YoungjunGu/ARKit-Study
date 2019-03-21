7cd2eb0ea954a6a097b218417100d90c4db9a323
gaki2745 <guyeongjun@naver.com>
2019-03-22 00:14:53 +0900

    Image Tracking

---
+++ ARReality/ARReality/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ARReality
+//
+//  Created by youngjun goo on 21/03/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var shapeNode = SCNNode()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shapeNode = SCNScene(named: "art.scnassets/kim.scn")!.rootNode.childNodes[0]
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+        
+        configuration.maximumNumberOfTrackedImages = 1
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+       return shapeNode
+        
+    }
+ 
+}
